50cbf3b82bea2acdb4215e5991b186f5b579dcd1
gtm.daemon <gtm.daemon@7dc7ac4e-7543-0410-b95c-c1676fc8e2a3>
2010-03-22 18:05:14 +0000

    [Author: dmaclach]
    Added some KVO debugging help. Specifically forces can access instance variables by KVO to NO
    in debug mode.
    
    R=thomasvl
    DELTA=340  (269 added, 21 deleted, 50 changed)

--- trunk/Foundation/GTMNSObject+KeyValueObserving.h
+++ trunk/Foundation/GTMNSObject+KeyValueObserving.h
@@ -32,6 +32,12 @@
 
 #import <Foundation/Foundation.h>
 
+#ifndef GTM_PERFORM_KVO_CHECKS
+// Controls whether KVO checking code is on in debug. See 
+// GTMNSObject+KeyValueObserving.m for details
+#define GTM_PERFORM_KVO_CHECKS 1
+#endif
+
 // If you read the articles above you will see that doing KVO correctly
 // is actually pretty tricky, and that Apple's documentation may not be
 // completely clear as to how things should be used. Use the methods below
@@ -51,6 +57,10 @@
 - (void)gtm_removeObserver:(id)observer 
                 forKeyPath:(NSString *)keyPath 
                   selector:(SEL)selector;
+
+// Use this to have |self| stop observing all keypaths on all objects.
+- (void)gtm_stopObservingAllKeyPaths;
+
 @end
 
 // This is the class that is sent to your notification selector as an
