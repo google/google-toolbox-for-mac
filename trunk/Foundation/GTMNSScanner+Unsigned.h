40073610416a4b5fe64b0bdc098bdaecc7733523
gtm.daemon <gtm.daemon@7dc7ac4e-7543-0410-b95c-c1676fc8e2a3>
2010-06-11 19:36:12 +0000

    [Author: thomasvl]
    Add missing include for 10.4 sdk.
    
    TBR=dmaclach

--- trunk/Foundation/GTMNSScanner+Unsigned.h
+++ trunk/Foundation/GTMNSScanner+Unsigned.h
@@ -6,9 +6,9 @@
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not
 //  use this file except in compliance with the License.  You may obtain a copy
 //  of the License at
-// 
+//
 //  http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 //  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -17,6 +17,8 @@
 //
 
 #import <Foundation/Foundation.h>
+
+#import "GTMDefines.h"
 
 // Adds support for working with unsigned values. Unsigned values are digits
 // only [0-9]. There is no support for "+" or "-" signs. Overflow is handled
