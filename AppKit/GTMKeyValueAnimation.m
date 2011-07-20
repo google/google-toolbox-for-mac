5db8e9f916b767abe787fc24ab1a0c801aa8bf07
gtm.daemon <gtm.daemon@7dc7ac4e-7543-0410-b95c-c1676fc8e2a3>
2011-07-20 22:00:40 +0000

    [Author: thomasvl]
    Fix typo.
    
    R=dmaclach
    DELTA=1  (0 added, 0 deleted, 1 changed)

--- AppKit/GTMKeyValueAnimation.m
+++ AppKit/GTMKeyValueAnimation.m
@@ -1,6 +1,6 @@
 //
 //  GTMKeyValueAnimation.m
-//  Copyright 20011 Google Inc.
+//  Copyright 2011 Google Inc.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not
 //  use this file except in compliance with the License.  You may obtain a copy
