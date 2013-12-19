61d6c3a9e9bf318b5e6059c0fb926037e42b4866
gtm.daemon <gtm.daemon@7dc7ac4e-7543-0410-b95c-c1676fc8e2a3>
2013-12-19 21:30:24 +0000

    Remove obsolete 64-bit NSNumber utility methods.
    DELTA=195  (9 added, 180 deleted, 6 changed)

--- trunk/Foundation/GTMNSNumber+64Bit.h
+++ trunk/Foundation/GTMNSNumber+64Bit.h
@@ -1,41 +1,8 @@
 //
-//  GTMNSNumber+64Bit.h
+// This file previously included utility methods for creating
+// 64-bit NSNumbers.
 //
-//  Copyright 2009 Google Inc.
+// Applications should be updated to use NSNumber literals and
+// boxed expressions, as described at
+// http://clang.llvm.org/docs/ObjectiveCLiterals.html
 //
-//  Licensed under the Apache License, Version 2.0 (the "License"); you may not
-//  use this file except in compliance with the License.  You may obtain a copy
-//  of the License at
-// 
-//  http://www.apache.org/licenses/LICENSE-2.0
-// 
-//  Unless required by applicable law or agreed to in writing, software
-//  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-//  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
-//  License for the specific language governing permissions and limitations under
-//  the License.
-//
-
-#import <Foundation/Foundation.h>
-#import "GTMDefines.h"
-#if GTM_IPHONE_SDK
-#import <CoreGraphics/CoreGraphics.h>
-#endif  // GTM_IPHONE_SDK
-
-// Adds support for working with NSIntegers, 
-// NSUIntegers, CGFloats and NSNumbers (rdar://5812091)
-@interface NSNumber (GTM64BitAdditions)
-
-+ (NSNumber *)gtm_numberWithCGFloat:(CGFloat)value;
-+ (NSNumber *)gtm_numberWithInteger:(NSInteger)value;
-+ (NSNumber *)gtm_numberWithUnsignedInteger:(NSUInteger)value;
-
-- (id)gtm_initWithCGFloat:(CGFloat)value NS_RETURNS_RETAINED NS_CONSUMES_SELF;
-- (id)gtm_initWithInteger:(NSInteger)value NS_RETURNS_RETAINED NS_CONSUMES_SELF;
-- (id)gtm_initWithUnsignedInteger:(NSUInteger)value NS_RETURNS_RETAINED NS_CONSUMES_SELF;
-
-- (CGFloat)gtm_cgFloatValue;
-- (NSInteger)gtm_integerValue;
-- (NSUInteger)gtm_unsignedIntegerValue;
-
-@end
