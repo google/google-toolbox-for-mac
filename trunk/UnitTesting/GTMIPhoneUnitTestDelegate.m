19a9545c0e96826f7458a1f836f0a10a4a45193e
gtm.daemon <gtm.daemon@7dc7ac4e-7543-0410-b95c-c1676fc8e2a3>
2014-03-26 12:00:19 +0000

    Pass correct UIApplication object to method runTestsAndExit:
    DELTA=2  (0 added, 0 deleted, 2 changed)

--- trunk/UnitTesting/GTMIPhoneUnitTestDelegate.m
+++ trunk/UnitTesting/GTMIPhoneUnitTestDelegate.m
@@ -71,7 +71,7 @@
 // Run through all the registered classes and run test methods on any
 // that are subclasses of SenTestCase. Terminate the application upon
 // test completion.
-- (void)applicationDidFinishLaunching:(UIApplication *)application {
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
 
   // We could get called twice once from our notification registration, and
   // once if we actually still are the delegate of the application after
@@ -87,7 +87,7 @@
   // To permit UI-based tests, run the tests and exit app in a delayed selector
   // call. This ensures tests are run outside of applicationDidFinishLaunching:.
   [self performSelector:@selector(runTestsAndExit:)
-             withObject:application
+             withObject:[notification object]
              afterDelay:0];
 }
 
