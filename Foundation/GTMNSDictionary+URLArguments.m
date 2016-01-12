2d213a306f611261d247d1520d3ef050c7f4ffea
Thomas Van Lenten <thomasvl@google.com>
2016-01-12 10:02:48 -0500

    Export unused symbols in category-only files for when they are used in static libs to avoid linker warnings

--- Foundation/GTMNSDictionary+URLArguments.m
+++ Foundation/GTMNSDictionary+URLArguments.m
@@ -6,9 +6,9 @@
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not
 //  use this file except in compliance with the License.  You may obtain a copy
 //  of the License at
-// 
+//
 //  http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 //  Unless required by applicable law or agreed to in writing, software
 //  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
 //  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
@@ -20,6 +20,12 @@
 #import "GTMNSString+URLArguments.h"
 #import "GTMMethodCheck.h"
 #import "GTMDefines.h"
+
+
+// Export a nonsense symbol to suppress a libtool warning when this is linked alone in a static lib.
+__attribute__((visibility("default")))
+    char GTMNSDictionaryURLArgumentsExportToSuppressLibToolWarning = 0;
+
 
 @implementation NSDictionary (GTMNSDictionaryURLArgumentsAdditions)
 
