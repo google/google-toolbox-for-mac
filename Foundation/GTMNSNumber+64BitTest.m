b1b88a27d579ccbf1062d19b9465bdf4a8cb8b25
Thomas Van Lenten <thomasvl@google.com>
2022-05-16 14:34:26 -0400

    Drop GTMNSNumber+64Bit, been empty a while. (#324)

--- Foundation/GTMNSNumber+64BitTest.m
+++
@@ -1,8 +0,0 @@
-//
-// This file previously included tests for utility methods
-// for creating 64-bit NSNumbers.
-//
-// Applications should be updated to use NSNumber literals
-// and boxed expressions, as described at
-// http://clang.llvm.org/docs/ObjectiveCLiterals.html
-//
